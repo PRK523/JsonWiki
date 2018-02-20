860cb62aebc62a8a7ad114809264d18476b19d4c
Pranoti Kulkarni <pranotikulkarni@viveks-air.fios-router.home>
2018-02-20 11:06:58 -0500

    Initial Commit

---
+++ JSONandSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONandSlider
+//
+//  Created by Pranoti Kulkarni on 2/20/18.
+//  Copyright © 2018 Pranoti Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
